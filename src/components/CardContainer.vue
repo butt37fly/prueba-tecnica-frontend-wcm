42c78ac123e7812094893a6019adde172e9fb8f6
Andrés Ospina <adrosscosmic@gmail.com>
2026-05-24 14:11:26 -0500

    feat: crear componente Card

---
+++ src/components/CardContainer.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+  body: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <article class="c-card w-full flex flex-col items-start p-4 gap-2" :data-id="props.id">
+    <h3 class="font-semibold text-lg">{{ props.title }}</h3>
+    <p>{{ props.body }}</p>
+  </article>
+</template>
+
+<style scoped>
+.c-card {
+  border: 1px solid var(--color-gray-500);
+  border-radius: var(--radius-sm);
+}
+</style>
