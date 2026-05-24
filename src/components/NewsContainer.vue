56481e81e07aeba7c7688de3898bdc9aac3b52c5
Andrés Ospina <adrosscosmic@gmail.com>
2026-05-24 14:11:45 -0500

    feat: crear componente News

---
+++ src/components/NewsContainer.vue
@@ -0,0 +1,22 @@
+<script setup>
+import CardContainer from './CardContainer.vue'
+
+const props = defineProps({
+  posts: {
+    type: Array,
+    default: null,
+  },
+})
+
+console.log(props.posts)
+</script>
+
+<template>
+  <section class="w-full" id="noticias">
+    <div class="grid max-w-285 m-auto grid-cols-1 gap-3 p-4 sm:grid-cols-2 lg:grid-cols-3">
+      <CardContainer v-for="post in props.posts" :key="post.id" :="post" />
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
